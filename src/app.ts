cc5247ddfc001cc656b3ae610a99ca805531250d
yijinc <yijinc@qq.com>
2023-06-16 17:59:27 +0800

    feat: use @lianqicloud/eslint-config

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
 import './app.scss'
 
-function App({ children }: PropsWithChildren) {
+function App({ children }: PropsWithChildren<any>) {
 
   useLaunch(() => {
     console.log('App launched.')
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,14 @@
-import { View, Text } from '@tarojs/components'
+import { Text, View } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
+    console.warn('Page loaded.')
   })
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
     </View>
   )
